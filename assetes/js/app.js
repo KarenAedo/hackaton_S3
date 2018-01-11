2bce29b8657cfd0b511d014207615203ed07055d
Vale <valeria.navarrete.h@gmail.com>
2018-01-11 15:08:46 -0300

    funcion para mostrar informacion de peliculas

--- assetes/js/app.js
+++ assetes/js/app.js
@@ -41,6 +41,7 @@
 //--------------------------------------------------------------------------------------
 
 
+
 var notice = data.notice.info;
 
 $('#btn-notice').click(function(){
@@ -105,3 +106,476 @@ $("#btn-profile").click(function(){
   $('.movies').children().remove();
   $('#my-data').show();
 });
+
+//funcion para mostrar informacion de las peliculas en un modal
+  $(".starWars").click(function(){
+    var movieId = "tt2527336";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".coco").click(function(){
+    var movieId = "tt2380307";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".logan").click(function(){
+    var movieId = "tt3315342";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".jumanji").click(function(){
+    var movieId = "tt2283362";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".yourName").click(function(){
+    var movieId = "tt5311514";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".apePlanet").click(function(){
+    var movieId = "tt3450958";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".fantasticBeasts").click(function(){
+    var movieId = "tt3183660";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
+
+  $(".furious").click(function(){
+    var movieId = "tt4630562";
+    $.ajax({
+      url: 'http://www.omdbapi.com?apikey=a839f700',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        i: movieId
+      },
+    })
+    .done(function(response) {
+      console.log(response);
+      var modalTitle = "";
+      var output = "";
+      for ( m in response) {
+         /* iterate through array or object */
+         var modalTitle =`
+        <div class="col-md-12">
+            <h2>${response.Title}</h2>
+          </div>
+      `; 
+
+         var output =`
+        <div class="row">
+          <div class="col-md-12">            
+            <img src="${response.Poster}" class="thumbnail">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Género:</strong> ${response.Genre}</li>
+              <li class="list-group-item"><strong>Duración:</strong> ${response.Runtime}</li>
+              <li class="list-group-item"><strong>Año:</strong> ${response.Year}</li>              
+              <li class="list-group-item"><strong>Director:</strong> ${response.Director}</li>
+              <li class="list-group-item"><strong>Actores:</strong> ${response.Actors}</li>
+              <li class="list-group-item"><strong>Website:</strong> <a href="${response.Website}" target="_blank">${response.Website}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Resumen</h3>
+            ${response.Plot}
+            <hr>
+            <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-bell"></span> Notificarme</a>
+          </div>
+        </div>
+      `;          
+
+      };
+      $(".modalTitle").html(modalTitle);
+      $(".descritionMovie").html(output);
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      //console.log("complete");
+    }); 
+  });
